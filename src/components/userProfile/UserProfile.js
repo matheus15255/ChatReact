74fabd7ddacd563a8d2ac642ca52fa6513e85383
matheus15255 <matheus_d.a.s@hotmail.com>
2021-09-09 23:46:36 -0300

    chatreact

---
+++ src/components/userProfile/UserProfile.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import "./userProfile.css";
+
+export default class UserProfile extends Component {
+  toggleInfo = (e) => {
+    e.target.parentNode.classList.toggle("open");
+  };
+  render() {
+    return (
+      <div className="main__userprofile">
+        <div className="profile__card user__profile__image">
+          <div className="profile__image">
+            <img src="https://ca.slack-edge.com/TLAVDH7C2-U029ZH1KB2Q-e7eab9943988-512" />
+          </div>
+          <h4>Matheus Alonso</h4>
+          <p>Estudante Full Stack Labenu</p>
+        </div>
+        <div className="profile__card">
+          <div className="card__header" onClick={this.toggleInfo}>
+            <h4>Information</h4>
+            <i className="fa fa-angle-down"></i>
+          </div>
+          <div className="card__content">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            ultrices urna a imperdiet egestas. Donec in magna quis ligula
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
